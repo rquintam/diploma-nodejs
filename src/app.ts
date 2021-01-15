bee716ba7df2c931b425b2e1b7a2358949541864
Rodrigo Quintam <rquintam@gmail.com>
2021-01-15 18:32:57 -0300

    Update App
    Import Cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 
 import express from 'express';
+import cors from 'cors';
 
 import routes from './routes';
 
@@ -8,6 +9,7 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
